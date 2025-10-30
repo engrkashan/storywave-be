9dc383f077f1104bf426afebfa40c1b4eea51104
Muh Qasim <muhammadqasimbhatti4@gmail.com>
2025-10-30 21:04:24 +0500

    :fire:Production and Local

--- src/services/inputService.js
+++ src/services/inputService.js
@@ -47,7 +47,7 @@ async function downloadVideo(url) {
     await ytdlp(url, {
       exec: "/root/.local/bin/yt-dlp", // 👈 use your working yt-dlp
       output: outputPath,
-      format: mp4, // ✅ safer than just mp4
+      format: "mp4", // ✅ safer than just mp4
       binaryPath:
         process.env.NODE_ENV === "production"
           ? "/root/.local/bin/yt-dlp"
