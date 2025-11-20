3dee236024bd31404b6888f6828f29d92b53411f
Muh Qasim <muhammadqasimbhatti4@gmail.com>
2025-11-20 14:51:52 +0500

    :fire:Subtitles

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -75,7 +75,7 @@ PlayResY: 1080
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,DejaVuSans,180,&H00FFFFFF&,&H00FFFFFF&,&H00000000&,&H00000000&,0,0,0,0,100,100,0,0,1,4,2,2,60,60,100,1
+Style: Default,DejaVuSans,80,&H00FFFFFF&,&H00FFFFFF&,&H00000000&,&H00000000&,0,0,0,0,100,100,0,0,1,4,2,2,60,60,100,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
